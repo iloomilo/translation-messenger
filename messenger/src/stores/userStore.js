3570f5d44249df72414745822e5f8ad64a3efe91
iloomilo <ilcantiryaki46@gmail.com>
2024-06-01 05:15:20 +0200

    Added friend modal and some boilerplate for socket.io rooms. Changed frontend for no friends and active chat

--- messenger/src/stores/userStore.js
+++ messenger/src/stores/userStore.js
@@ -1,5 +1,6 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import socket from "../socket.js";
 
 export const useUserStore = defineStore('userStore', () => {
     
@@ -7,6 +8,10 @@ export const useUserStore = defineStore('userStore', () => {
   
     const authorizeUser = (loggedInUser) => {
       user.value = loggedInUser;
+      socket.auth = user.value;
+      socket.connect();
+      console.log(socket);
+      console.log(socket.auth)
     }
 
     return { user, authorizeUser }
